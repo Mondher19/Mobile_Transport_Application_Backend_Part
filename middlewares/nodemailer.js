e59f236abf7e84a18f828fe4354d6ec73d6da844
Mondher19 <99404210+Mondher19@users.noreply.github.com>
2023-05-09 19:33:59 +0100

    Update nodemailer.js

--- middlewares/nodemailer.js
+++ middlewares/nodemailer.js
@@ -1,33 +1,41 @@
 import nodemailer from "nodemailer";
-import smtpTransport from 'nodemailer-smtp-transport';
+import env from 'dotenv';
 
-export async function sendConfirmationEmail(email, activationCode) {
+
+
+
+
+export async function  sendConfirmationEmail (email,activationCode)  {
+ 
   try {
-    
-    const transporter = nodemailer.createTransport(smtpTransport({
-  host: 'live.smtp.mailtrap.io',
-  port: 587,
-  secure: true,
-  auth: {
-    user: 'api',
-    pass: 'b6466441c473c50bd104fcacaea2e8f1',
-  },
-    }));
+    const transporter = nodemailer.createTransport({
 
+      service: "Gmail",
+      auth: { 
+
+        user:"hamzabenayed786@gmail.com",
+        pass: "sdwmbcilfnjwkkgp", 
+      },
+     // tls: {rejectUnauthorized: false}
+ 
+    });
+    const listofnumber = "0123456789";
+    
     await transporter.sendMail({
-      from: 'live.smtp.mailtrap.io',
+
+     from: "hamza.benayed@esprit.tn",
       to: email,
-      subject: 'Please confirm your account',
-      html: `<h1> This is your code </h1>
-        <h2>Hello Sir </h2>
+      subject: "Please confirm your account",
+      text: "Email Confirmation " ,
+       html: `<h1> Hi ! This is your confirmation code </h1>
         <h3> ${activationCode}</h3>`,
     });
-
-    console.log('email sent successfully');
+    
+    console.log("email sent sucessfully");
   } catch (error) {
-    console.log('email not sent');
+    console.log("email not sent");
     console.log(error);
   }
-}
+};
 
 export default sendConfirmationEmail;
